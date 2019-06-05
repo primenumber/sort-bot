d0334f11a65226472bd2e2af6265d19f70960dd5
primenumber <prime@kmc.gr.jp>
2019-06-05 16:38:56 +0900

    Fix for new sort bot

--- main.rb
+++ main.rb
@@ -13,35 +13,91 @@ $client.auth_test
 
 $n = File.new("size.txt").gets.to_i
 
-$list = Array.new($n) {|i| i}
-$list.shuffle!
-
-def sorted?
-  $list.each_cons(2).all? {|a, b| a <= b}
-end
-
 def inc_size
   $n += 1
-  $list = Array.new($n) {|i| i}
-  $list.shuffle!
   File.open("size.txt", "w") do |f|
     f.puts($n.to_s)
   end
 end
 
-def post_slack
-  $client.chat_postMessage(channel: '#bogosort', text: $list.to_s, as_user: true)
+def post_list(list)
+  $client.chat_postMessage(channel: '#sort-algorithms', text: list.to_s, as_user: true)
 end
 
-def bogosort
-  $list.shuffle!
-  post_slack
-  if sorted? then
-    inc_size
+def post_algo(algo)
+  $client.chat_postMessage(channel: '#sort-algorithms', text: "#{algo.name}, n=#{$n}", as_user: true)
+end
+
+class SortMachine
+  def sort(list)
+    raise NotImplementedError.new("You must implement #{self.class}##{__method__}")
   end
 end
 
+class BubbleSort < SortMachine
+  def sort(list)
+    n = list.length
+    n.times do |i|
+      (n-i-1).times do |j|
+        if list[j] > list[j+1] then
+          list[j], list[j+1] = list[j+1], list[j]
+        end
+        yield list
+      end
+    end
+    list
+  end
+end
+
+class QuickSort < SortMachine
+  def sort(list)
+    if list.length < 2 then
+      yield list
+      return list
+    end
+    if list.length == 2 then
+      if list[0] > list[1] then
+        list[0], list[1] = list[1], list[0]
+      end
+      yield list
+      return list
+    end
+    copy = Array.new(list)
+    pivot = copy.shift
+    larges = []
+    smalls = []
+    copy.each {|e|
+      if e > pivot then
+        larges.push(e)
+      else
+        smalls.push(e)
+      end
+    }
+    yield smalls + [pivot] + larges
+    smalls = sort(smalls) {|tmp|
+      yield tmp + [pivot] + larges
+    }
+    larges = sort(larges) {|tmp|
+      yield smalls + [pivot] + tmp
+    }
+    smalls + [pivot] + larges
+  end
+end
+
+$algorithms = [BubbleSort, QuickSort]
+
 loop do
-  bogosort
-  sleep(60)
+  $algorithms.each {|algo|
+    post_algo(algo)
+    obj = algo.new
+    list = Array.new($n) {|i| i}
+    list.shuffle!
+    post_list(list)
+    sleep(60)
+    obj.sort(list) {|tmp|
+      post_list(tmp)
+      sleep(60)
+    }
+  }
+  inc_size
 end
